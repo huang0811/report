81d0d5fd45b09dbb9dbe7234af7c7b67aa3db30e
huang0811 <s1100393@gm.pu.edu.tw>
2022-12-22 23:23:09 +0800

    make it better

--- index.py
+++ index.py
@@ -1,5 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime, timezone, timedelta
@@ -21,8 +27,17 @@ def webhook():
     #info = "動作：" + action + "； 查詢內容：" + msg
     if (action == "drinkChoice"):
         fange =  req.get("queryResult").get("parameters").get("fange")
-        info = "您想查詢的飲料是：" + fange
-    return make_response(jsonify({"fulfillmentText": info}))
-
+        info = "您想查詢的飲料是：" + fange + "，相關飲品:\n"
+    
 #if __name__ == "__main__":
 #    app.run()
+    collection_ref = db.collection("drink")
+    docs = collection_ref.get()
+    result = ""
+    for doc in docs:
+        dict = doc.to_dict()
+        if fange in dict["name"]:
+            result += "飲料:" + dict["name"] + "\n"
+            result += "價錢:" + dict["price"] + "\n"
+        info += result
+    return make_response(jsonify({"fulfillmentText": info}))
