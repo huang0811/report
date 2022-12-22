1cf89ddc3559eb3273cbb2fd63bd5e7ac24a41f2
huang0811 <s1100393@gm.pu.edu.tw>
2022-12-22 19:19:28 +0800

    make it better

--- index.py
+++ index.py
@@ -17,8 +17,11 @@ def webhook():
     req = request.get_json(force=True)
     # fetch queryResult from json
     action =  req.get("queryResult").get("action")
-    msg =  req.get("queryResult").get("queryText")
-    info = "動作：" + action + "； 查詢內容：" + msg
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "動作：" + action + "； 查詢內容：" + msg
+    if (action == "fangeChoice"):
+        rate =  req.get("queryResult").get("parameters").get("fange")
+        info = "您想查詢的飲料是：" + fange
     return make_response(jsonify({"fulfillmentText": info}))
 
 #if __name__ == "__main__":
